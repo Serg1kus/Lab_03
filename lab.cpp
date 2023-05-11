70778f082e328822d4c63c4e917f772e15d70f40
Serg1kus <114881841+Serg1kus@users.noreply.github.com>
2023-05-11 23:28:42 +0300

    lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    long long first_term = 1; 
-    long long common_difference = 1; 
-    long long last_term = 100000000000; 
-    long long current_term = first_term; 
+    int power = 3;             
+    long long product = 1;     
 
-    while(current_term <= last_term)
+    for(int i = 2; i <= 1000; i += 2)  
     {
-        
-        current_term += common_difference; 
+        int powered_num = pow(i, power);    
+        product *= powered_num;             
     }
+
+    cout << "The product of even numbers from 1 to 10000 raised to the power of " << power << " is: " << product << endl;
 
     return 0;
 }
