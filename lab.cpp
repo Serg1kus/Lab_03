4ae6e8646ba114ba92c0867e87c16480f8a3f48e
Serg1kus <114881841+Serg1kus@users.noreply.github.com>
2023-05-12 00:14:42 +0300

    lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -3,47 +3,21 @@
 
 using namespace std;
 
-// A function to multiply two numbers represented as vectors
-vector<int> multiply(vector<int> num1, vector<int> num2) {
-    vector<int> result(num1.size() + num2.size(), 0);
-    int carry = 0;
-    for (int i = num2.size() - 1; i >= 0; i--) {
-        carry = 0;
-        for (int j = num1.size() - 1; j >= 0; j--) {
-            int prod = num1[j] * num2[i] + carry + result[i + j + 1];
-            carry = prod / 10;
-            result[i + j + 1] = prod % 10;
-        }
-        result[i] = carry;
-    }
-    while (result.size() > 1 && result[0] == 0) {
-        result.erase(result.begin());
-    }
-    return result;
-}
-
-// A function to calculate factorial of a number
-vector<int> factorial(int n) {
+// A function to calculate the geometric progression from 1 to n
+vector<int> geometricProgression(int n) {
     vector<int> result = {1};
-    for (int i = 2; i <= n; i++) {
-        vector<int> num(i / 10 + 1, 0);
-        int j = i;
-        int k = 0;
-        while (j > 0) {
-            num[k++] = j % 10;
-            j /= 10;
-        }
-        result = multiply(result, num);
+    for (int i = 1; i <= n; i++) {
+        result.push_back(result.back() * (i - 1));
     }
     return result;
 }
 
 int main() {
-    for (int i = 0; i <= 1000; i++) {
-        vector<int> result = factorial(i);
-        //cout << i << "! = ";
+    for (int i = 1; i <= 100; i++) {
+        vector<int> result = geometricProgression(i);
+        //cout << "GP(" << i << ") = ";
         //for (int j = 0; j < result.size(); j++) {
-            //cout << result[j];
+            //cout << result[j] << " ";
         //}
         //cout << endl;
     }
