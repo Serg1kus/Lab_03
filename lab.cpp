5093d750ba875475714c3cd5c9666d93b59a7f04
Serg1kus <114881841+Serg1kus@users.noreply.github.com>
2023-05-11 18:57:04 +0300

    lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -6,10 +6,12 @@ int main()
 {
     long long int product = 1;
 
-    for(int i = 1; i <= 1000000000000; i++)
+    for(int i = 1; i <= 1000; i++)
     {
         product *= i;
     }
+
+    cout << "Product of numbers from 1 to 1000000 is: " << product << endl;
 
     return 0;
 }
